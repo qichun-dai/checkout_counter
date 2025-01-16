ef2298a44dc19ea83dc30211b1e09d644e9d2e22
Qichun Dai <qichun.dai@gmail.com>
2025-01-16 17:10:31 +0100

    created the bottons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-//import CheckoutCounter from './CheckoutCounter';
+import React from "react";
+import Heading from "./CheckoutCounter";
+import CheckoutCounter from "./CheckoutCounter";
 
-function App(){
-    return <div>
-        <h1>Checkout Counter</h1>
+function App() {
+  const [products, setProducts] = React.useState([
+    { id: 1, name: "Apple", price: 0.5, quantity: 10 },
+    { id: 2, name: "Banana", price: 0.3, quantity: 5 },
+    { id: 3, name: "Cherry", price: 0.1, quantity: 1 },
+  ]);
+
+  return (
+    <div className="main">
+      <h1>Checkout Counter</h1>
+      {products.map((product) => (
+        <CheckoutCounter
+          key={product.id}
+          defaultCount={product.quantity}
+          onDeleteClick={() =>
+            setProducts((products) =>
+              products.filter((p) => p.id !== product.id)
+            )
+          }
+        />
+      ))}
     </div>
+  );
 }
 
 export default App;
