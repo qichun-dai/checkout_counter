40aed9c8bf03982b54d583620ad5bc17dbde02b7
Qichun Dai <qichun.dai@gmail.com>
2025-01-16 15:42:23 +0100

    update v1

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+//import CheckoutCounter from './CheckoutCounter';
+
+function App(){
+    return <div>
+        <h1>Checkout Counter</h1>
+    </div>
+}
+
+export default App;
