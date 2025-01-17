b7d3f9bc0b245b1909dac384c4488f4e93faf1d7
Qichun Dai <qichun.dai@gmail.com>
2025-01-17 15:21:17 +0100

    update styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,15 @@ import CheckoutCounter from "./CheckoutCounter";
 
 function App() {
   const [products, setProducts] = React.useState([
-    { id: 1, name: "Apple", price: 0.5, quantity: 10 },
-    { id: 2, name: "Banana", price: 0.3, quantity: 5 },
-    { id: 3, name: "Cherry", price: 0.1, quantity: 1 },
+    { id: 1, name: "Apple", price: 2.5, quantity: 99 },
+    { id: 2, name: "Banana", price: 1.3, quantity: 5 },
+    { id: 3, name: "Cherry", price: 5.1, quantity: 1 },
   ]);
 
   return (
     <div className="main">
       <h1>Shopping Cart</h1>
-      {products.map((product) => (<>
-        {product.name} - ${product.price.toFixed(2)} per item
+      {products.map((product) => (
         <CheckoutCounter
           key={product.id}
           defaultCount={product.quantity}
@@ -22,7 +21,7 @@ function App() {
             )
           }
         />
-      </>))}
+      ))}
     </div>
   );
 }
